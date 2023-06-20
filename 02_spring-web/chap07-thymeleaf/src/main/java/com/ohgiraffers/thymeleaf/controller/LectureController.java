f87550ca56a84dcb113cbed8ae45c9e4c960f299
nonamugu <0908jhk@naver.com>
2023-06-20 13:07:37 +0900

    20230618 Spring Thymeleaf commit

---
+++ 02_spring-web/chap07-thymeleaf/src/main/java/com/ohgiraffers/thymeleaf/controller/LectureController.java
@@ -0,0 +1,84 @@
+package com.ohgiraffers.thymeleaf.controller;
+
+import com.ohgiraffers.thymeleaf.model.dto.MemberDTO;
+import com.ohgiraffers.thymeleaf.model.dto.SelectCriteria;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Controller
+@RequestMapping("/lecture")
+public class LectureController {
+
+    @GetMapping("/expression")
+    public ModelAndView expression(ModelAndView mv) {
+        mv.addObject("member", new MemberDTO("홍길동", 20, '남', "서울시 서초구"));
+
+        // ModelAndView의 Model에 추가
+        mv.addObject("hello", "hello!<h3>Thymeleaf</h3>");
+
+        mv.setViewName("/lecture/expression");
+
+        return mv;
+    }
+
+    @GetMapping("/conditional")
+    public ModelAndView conditional(ModelAndView mv) {
+        // ModelAndView의 Model에 추가
+        mv.addObject("num", 1);
+        mv.addObject("str", "바나나");
+
+        // ModelAndView의 Model에 추가 (name, age, gender, address)
+        List<MemberDTO> memberList = new ArrayList<>();
+        memberList.add(new MemberDTO("홍길동", 20, '남', "서울시 서초구"));
+        memberList.add(new MemberDTO("유관순", 22, '여', "서울시 노원구"));
+        memberList.add(new MemberDTO("장보고", 40, '남', "서울시 종로구"));
+        memberList.add(new MemberDTO("신사임당", 30, '여', "서울시 성북구"));
+        mv.addObject("memberList", memberList);
+
+        mv.setViewName("/lecture/conditional");
+
+        return mv;
+    }
+
+    @GetMapping("/etc")
+    public ModelAndView etc(ModelAndView mv) {
+        // ModelAndView의 Model에 추가 (startPage, endPage, pageNo)
+        SelectCriteria selectCriteria = new SelectCriteria(1, 10, 3);
+        mv.addObject(selectCriteria);
+
+        // ModelAndView의 Model에 추가 (name, age, gender, address)
+        MemberDTO member = new MemberDTO("홍길동", 20, '남', "서울시 서초구");
+
+        mv.addObject("member", member);
+
+        List<MemberDTO> memberList = new ArrayList<>();
+        memberList.add(new MemberDTO("홍길동", 20, '남', "서울시 서초구"));
+        memberList.add(new MemberDTO("유관순", 22, '여', "서울시 노원구"));
+        memberList.add(new MemberDTO("장보고", 40, '남', "서울시 종로구"));
+        memberList.add(new MemberDTO("신사임당", 30, '여', "서울시 성북구"));
+
+        mv.addObject("memberList", memberList);
+
+        Map<String, MemberDTO> memberMap = new HashMap<>();
+        memberMap.put("m01", new MemberDTO("홍길동", 20, '남', "서울시 서초구"));
+        memberMap.put("m02", new MemberDTO("유관순", 22, '여', "서울시 노원구"));
+        memberMap.put("m03", new MemberDTO("장보고", 40, '남', "서울시 종로구"));
+        memberMap.put("m04", new MemberDTO("신사임당", 30, '여', "서울시 성북구"));
+
+        mv.addObject("memberMap", memberMap);
+
+        mv.setViewName("/lecture/etc");
+
+        return mv;
+    }
+
+
+}
