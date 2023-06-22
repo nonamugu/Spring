354e3a5b06801053886b1f8f290c9157a323ba72
nonamugu <0908jhk@naver.com>
2023-06-22 15:10:23 +0900

    20230622 Jpa Mapping commit

---
+++ 03_jpa/chap02-mapping/src/test/java/com/ohgiraffers/section06/compositekey/subsection01/embedded/MemberPK.java
@@ -0,0 +1,51 @@
+package com.ohgiraffers.section06.compositekey.subsection01.embedded;
+
+import org.hibernate.engine.spi.Mapping;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import javax.persistence.EmbeddedId;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Embeddable
+public class MemberPK implements Serializable {
+    @Column(name="member_no")
+    private int memberNo;
+    @Column(name="member_id")
+    private String memberId;
+    public MemberPK() {}
+    public MemberPK(int memberNo, String memberId) {
+        super();
+        this.memberNo = memberNo;
+        this.memberId = memberId;
+    }
+    public int getMemberNo() {
+        return memberNo;
+    }
+    public void setMemberNo(int memberNo) {
+        this.memberNo = memberNo;
+    }
+    public String getMemberId() {
+        return memberId;
+    }
+    public void setMemberId(String memberId) {
+        this.memberId = memberId;
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        MemberPK memberPK = (MemberPK) o;
+        return memberNo == memberPK.memberNo && Objects.equals(memberId, memberPK.memberId);
+    }
+    @Override
+    public int hashCode() {
+        return Objects.hash(memberNo, memberId);
+    }
+    @Override
+    public String toString() {
+        return "MemberPK [memberNo=" + memberNo + ", memberId=" + memberId + "]";
+    }
+
+}
